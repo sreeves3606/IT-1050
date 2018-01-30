46d841e1ee6640494acdfd185779ca0bdfd07fcd
sreeves3606 <35580478+sreeves3606@users.noreply.github.com>
2018-01-30 16:54:35 -0500

    Lab01 Final Copy

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string person = "Stephanie Reeves";
+            Console.WriteLine("Hello World from " + person);
+            Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();
+
+        }
+    }
+}
