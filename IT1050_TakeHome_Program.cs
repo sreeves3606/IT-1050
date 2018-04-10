3aaca08374fa8232efb76358a25455c4f4a54a23
Stephanie Reeves <35580478+sreeves3606@users.noreply.github.com>
2018-04-10 00:50:46 -0400

    Add files via upload

---
+++ IT1050_TakeHome_Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT1050_TakeHome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         /*    bool keepLooping = true; // Infinite While Loop Example
+               while (keepLooping)
+               {
+                   Console.WriteLine("keepLooping"); // use Ctl + C to terminate program
+               }
+
+           }
+       }
+   } 
+              int x = 2; // While Loop Prints 2 - 128
+              while (x <= 128)
+              {
+                  Console.WriteLine("[{0}]", x);
+                  x += 2;
+              }
+          }
+      }
+  } 
+              for (int i = 49; i > 0; i--) // For Loop 49-1
+              {
+                  Console.Write(i);
+                  if (i != 1)
+                  {
+                      Console.Write(",");
+                  }
+              }
+          }
+      }
+  } 
+              int i = 1; // While Loop Prints 1-21 With 3 Spaces
+              while (i <= 21)
+              {
+                  if (i % 2 != 0)
+                  {
+                      Console.Write(i + "   ");
+                      i++;
+                  }
+                  else
+                  {
+                      i++;
+                  }
+              }
+          }
+      }
+  } 
+            int n = 8; // Implement Code Using a While Loop
+            int i = 10; // initialize
+            do
+            {
+                Console.Write("*");
+                i++; // Update!
+            } while (i < n); // test condition
+         }
+    }
+} 
+   // The do-while output is only one asterisk. The while statement does not have any output. 
+  //  The while loop checks the test condition before running the program. 
+ //   The do-while loop runs the program first, then checks the test condition.
+
+        bool icyRain = false; // Boolean Program
+        bool tornadoWarning = false;
+        if (icyRain == false && tornadoWarning == false)
+        {
+            Console.WriteLine("Let's go outside!");
+        }
+    }
+ }
+} 
+            int numberoflayer = 5, Space, Number; // Hoping for Partial Credit on the Extra Credit
+            for (int i = 1; i <= numberoflayer; i++)
+            {
+            for (Space = 1; Space <= (numberoflayer - i); Space++)
+                    Console.Write(" ");
+            for (Number = 1; Number <= i; Number++)
+                        Console.Write(Number);
+            for (Number = (i - 1); Number >= 1; Number--)
+                    Console.Write(Number);
+                    Console.Write(" ");
+                }
+            }
+        }
+    } */
+
+
+        
+
