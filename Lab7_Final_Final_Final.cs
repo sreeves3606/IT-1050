fcba2e3ba5b0bf2c81214510700ddeab1a4052ac
Stephanie Reeves <35580478+sreeves3606@users.noreply.github.com>
2018-04-12 01:47:21 -0400

    Add files via upload

---
+++ Lab7_Final_Final_Final.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB7_Final_Final_Final
+{
+    class MyMath
+    {
+        static void Main(string[] args)
+        {
+            //User enters two numbers
+            Console.Write("Enter first number: ");
+            double operand1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            double operand2 = double.Parse(Console.ReadLine());
+
+            //Addition Method
+            double result = operand1 + operand2; 
+            Console.WriteLine(result);
+
+            //Multiplication Method
+            double result1 = operand1 * operand2;
+            Console.WriteLine(result1);
+
+            //Subtraction Method
+            double result2 = operand1 - operand2;
+            Console.WriteLine(result2);
+
+            //Division Method
+            double result3 = operand1 / operand2;
+            Console.WriteLine(result3);
+        }
+        
+       
+    }
+}
+            
