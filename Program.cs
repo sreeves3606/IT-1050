6d3b3611487c774feeae6521d84a58d123d3a329
Stephanie Reeves <35580478+sreeves3606@users.noreply.github.com>
2018-05-01 14:02:07 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,47 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab03_StephanieReves
+namespace LastLab_IT1050
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            int numberA; // first number declared
-            int numberB; // second number declared
-            int sum; // sum of first and second number declared
+            int car1Speed = 20;
+            int car2Speed = 0;
+            Car car1 = new Car("Ford", "Focus", 2010, car1Speed);
+            Car car2 = new Car("Chevy", "Cruze", 2018, car2Speed);
 
-            Console.Write("Enter first number: "); // user promted for first number
-            numberA = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 60; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    car2Speed = car2.SpeedUp();
+                }
+                if (i % 3 == 0)
+                {
+                    car1Speed = car1.SpeedUp();
+                }
+                if (i % 5 == 0)
+                {
+                    car1Speed = car1.SlowDown();
+                    car2Speed = car2.SlowDown();
+                }
+            }
+            car1.Display();
+            car2.Display();
 
-            Console.Write("Enter second number: "); // user prompted for second number
-            numberB = int.Parse(Console.ReadLine());
-
-            sum = numberA + numberB; // first and second numbers added
-            Console.WriteLine($"Sum is {sum}");  // resulting sum displayed
-
-            int x; // first variable declared
-            int y; // second variable declared
-            int z; // third variable declared
-            int result; // result of variables
-
-            Console.Write("Enter number for x: "); // user prompted for x
-            x = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter number for y: "); // user prompted for y
-            y = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter number for z: "); // user prompted z
-            z = int.Parse(Console.ReadLine());
-
-            result = (x + y) * (z + 10);
-            Console.WriteLine($"Product is {result}");  // resulting sum displayed
-
-            Console.WriteLine("Hello\tWorld!");
-            Console.WriteLine("Hello\nWorld");
-            Console.WriteLine("\"Hello World!\"");
-            Console.WriteLine("Hello\\World");
-               
         }
     }
 }
