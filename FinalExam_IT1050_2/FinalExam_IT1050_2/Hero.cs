62a48cc1ae6eab082757f60ade6a5fa86656221f
Stephanie Reeves <35580478+sreeves3606@users.noreply.github.com>
2018-05-08 03:30:31 -0400

    Add files via upload

---
+++ FinalExam_IT1050_2/FinalExam_IT1050_2/Hero.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam_IT1050_2
+{
+    class Hero
+    {
+        private string Alterego;
+        private string Weakness;
+        private bool Antihero;
+        private string Gender;
+        private string Race;
+
+        public string Power;
+        public int PowerLevel;
+        public double Health;
+        public string Weapons;
+        public int Age;
+        public string Name;
+        public string nemesis;
+
+        public Hero()
+        {
+        }
+
+        public Hero(string name, string nemesis, string race, string power)
+        {
+            this.Name = name;
+            this.nemesis = nemesis;
+            this.Race = race;
+            this.Power = power;
+        }
+
+        public Hero(string name, string nemesis, string race, string power, bool antihero)
+        {
+            this.Name = name;
+            this.nemesis = nemesis;
+            this.Race = race;
+            this.Power = power;
+            this.Antihero = antihero;
+        }
+
+        /*TODO: Problem 1 - add a constructor method with a different 
+        number and/or types of parameters*/
+
+        public Hero(string lastName, string firstName)
+        {
+            string last = lastName;
+            string first = firstName;
+        }
+
+        /* TODO: Problem 2 - Output "Successfully fought crime at address" if 
+        PowerLevel > 49 */
+
+        public void FightCrime(string address)
+        {
+            if (PowerLevel > 49)
+            { Console.WriteLine("Successfully fought crime at " + address); }
+        }
+
+        // TODO: Problem 3 - output "Investigated Crime" and return true
+
+        public bool Investigate()
+        {
+            bool returnValue = true;
+
+            if (returnValue == true)
+            { Console.WriteLine("Investigated Crime"); }
+            return returnValue;
+        }
+
+        // TODO: Problem 4 - if Power contains Fly, then output "Name is Flying!" else output "Name can't fly!"
+        public void Fly()
+        {
+            if (this.Power.Contains("Fly"))
+            {
+                Console.WriteLine(Name + " is flying!");
+            }
+            else
+            { Console.WriteLine(Name + " can't fly!"); }
+        }
+
+        public void Craft(string material, int numMaterials)
+        {
+            Console.WriteLine("Crafting with " + material);
+        }
+
+        public void Climb(int height)
+        {
+            Console.WriteLine(this.Name + " is climbing");
+        }
+
+        // TODO: Problem 5 - increase this.PowerLevel by 1
+        public int ChargePower()
+        {
+            if (PowerLevel <= 49)
+            { PowerLevel++; }
+            return PowerLevel;
+        }
+
+        // TODO: Problem 6 - Add an overloaded method ChargePower that has a parameter for increaseAmount.  Add increaseAmount to this.PowerLevel and return the new value for this.PowerLevel (try to use a loop calling the other ChargePower)
+
+        public int ChargePower(int increaseAmount)
+        {
+            PowerLevel = PowerLevel + increaseAmount;
+
+            Console.WriteLine(PowerLevel);
+            return PowerLevel;
+        }
+    }
+}
+
---
+++ FinalExam_IT1050_2/FinalExam_IT1050_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam_IT1050_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero myHero = new Hero("Panther", "WhoopA", "Lil Joker", "strength, speed, golden chalice, Wonder Twins activate", true);
+            Hero myOtherHero = new Hero("Flash", "Zoom", "Human", "Speed", true);
+            Hero thirdHero = new Hero("Wonder Woman", "All bad people", "Amazonian", "Stength, Speed, Fly");
+
+            myHero.PowerLevel = 49;
+            myOtherHero.PowerLevel = 49;
+            thirdHero.PowerLevel = 49;
+
+             /*
+                Problem 7:
+                Each Hero must:
+                 - ChargePower (as much as you want)
+                 - Investigate
+                 - FightCrime (at least one must win. at least one must not win)
+                 - Fly (at least one superhero should be able to fly)
+                 - Climb
+            */
+
+            myHero.ChargePower(50);
+            myOtherHero.ChargePower(50);
+            thirdHero.ChargePower();
+
+            myHero.Investigate();
+            myOtherHero.Investigate();
+            thirdHero.Investigate();
+
+            myHero.FightCrime("the Halls of Justice");
+            myOtherHero.FightCrime("the creepy house on Mockingbird Lane");
+            thirdHero.FightCrime("the Skid Row Apartments");
+
+            myHero.Fly();
+            myOtherHero.Fly();
+            thirdHero.Fly();
+
+            myHero.Climb(50);
+            myOtherHero.Climb(20);
+            thirdHero.Climb(25);
+
+            Console.ReadKey();
+
+        }
+    }
+}
+       
